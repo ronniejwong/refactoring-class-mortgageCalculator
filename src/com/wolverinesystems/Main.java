1bcc76ab6d00f0b741f1c373f0162953fc34f375
nocturnalhippoz <wolverinesystems@gmail.com>
2020-02-22 23:36:37 -0500

    refactored scanner to Console class

--- src/com/wolverinesystems/Main.java
+++ src/com/wolverinesystems/Main.java
@@ -1,18 +1,15 @@
 package com.wolverinesystems;
 
-import com.sun.source.doctree.SystemPropertyTree;
-
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
-        int principal = (int)readNumber("Principal: ", 1000, 1_000_000);
-        float annualInterest = (float)readNumber("Annual Interest Rate: ", 1, 30);
-        byte years = (byte)readNumber("Period (Years): ", 1, 30);
+        int principal = (int) Console.readNumber("Principal: ", 1000, 1_000_000);
+        float annualInterest = (float) Console.readNumber("Annual Interest Rate: ", 1, 30);
+        byte years = (byte) Console.readNumber("Period (Years): ", 1, 30);
 
         var mortgage1 = new Mortgage(principal, annualInterest, years);
+        var mortgage2 = new Mortgage(principal, annualInterest, years);
 
         System.out.println("Number of Mortgages: " + Mortgage.numberOfMortgages);
 
@@ -25,16 +22,4 @@ public class Main {
         mortgage1.printBalance();
     }
 
-    public static double readNumber(String prompt, double min, double max){
-        Scanner scanner = new Scanner(System.in);
-        double value;
-        while (true){
-            System.out.println(prompt);
-            value = scanner.nextFloat();
-            if (value >= min && value <= max)
-                break;
-            System.out.println("Enter a value between " + min + " and " + max);
-        }
-        return value;
-    }
 }
